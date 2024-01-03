3fd4d2f40918748335d116c250e43cf988b3a60b
Rafa Sinhoroto <rsinhoroto@yahoo.com>
2024-01-03 16:23:49 -0300

    Changed exclamation mark to question mark, implemented form validation

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-function change(){
+function reset(){
     var body=document.querySelector('body')
-    body.style.backgroundColor="#301295"    
+    body.style.backgroundColor="ivory"
+
+    var texto = document.querySelector('p')
+    texto.style.display = "none"
 }
 
 var secretbutton = document.getElementById("birthdate")
@@ -11,26 +14,30 @@ secretbutton.addEventListener("keyup", function(e){
 })
 
 function submit(){
-    var color
-    var rawdata = document.getElementById("birthdate")
-    var data = new Date(rawdata.value+"T00:00:00")
+    var rawdata = document.getElementById("birthdate").value
+    if (!rawdata) return
+
+    var data = new Date(rawdata + "T00:00:00")
     var options = document.getElementsByName("format")
     var body=document.querySelector('body')
     var r = g = b = 0
-    //TODO: add birth date value validation
-    if (options[0].checked){
+
+    if (options[0].checked){            // format: dd/mm/yyyy
         r = Math.ceil(((data.getDate()-1)/30)*255)
         g = Math.ceil((data.getMonth()/11)*255)
         b = Math.ceil(((data.getFullYear()%100)/99)*255)
-    } else if (options[1].checked){
-        g = Math.ceil(((data.getDate()-1)/30)*255)
+
+    } else if (options[1].checked){     // format: mm/dd/yyyy
         r = Math.ceil((data.getMonth()/11)*255)
+        g = Math.ceil(((data.getDate()-1)/30)*255)
         b = Math.ceil(((data.getFullYear()%100)/99)*255)
-    } else {
-        b = Math.ceil(((data.getDate()-1)/30)*255)
-        g = Math.ceil((data.getMonth()/11)*255)
+
+    } else {                            // format: yyyy/mm/dd
         r = Math.ceil(((data.getFullYear()%100)/99)*255)
+        g = Math.ceil((data.getMonth()/11)*255)
+        b = Math.ceil(((data.getDate()-1)/30)*255)
     }
+    
     body.style.backgroundColor=`rgb(${r},${g},${b})`
     var texto = document.querySelector('p')
     texto.style.display = "block"
